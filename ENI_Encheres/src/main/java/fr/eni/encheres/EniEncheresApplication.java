3ffc7b930cc6b34007f13e0dc71d0e325ae0d300
nsimon2023 <nsimon2023@12SE22-1ZLRZS2.ad.campus-eni.fr>
2023-06-26 10:17:47 +0200

    test first

---
+++ ENI_Encheres/src/main/java/fr/eni/encheres/EniEncheresApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniEncheresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniEncheresApplication.class, args);
+	}
+
+}
---
+++ ENI_Encheres/src/test/java/fr/eni/encheres/EniEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniEncheresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
