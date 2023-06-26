726e1c51d9a302ed70b7b2b585cea0c4e429ca75
JuOkami <justin.candela@gmail.com>
2023-06-26 16:34:44 +0200

    Creation du Artciles Row Mapper

---
+++ ENI_Encheres/src/main/java/fr/eni/encheres/dao/ArticleVenduRowMapper.java
@@ -0,0 +1,44 @@
+package fr.eni.encheres.dao;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import fr.eni.encheres.bo.ArticleVendu;
+
+public class ArticleVenduRowMapper implements RowMapper<ArticleVendu>{
+	
+	EncheresDaoArticlesVendus encheresDaoArticlesVendus;
+	EncheresDaoCategories encheresDaoCategories;
+	
+	
+
+	public ArticleVenduRowMapper(EncheresDaoArticlesVendus encheresDaoArticlesVendus, EncheresDaoCategories encheresDaoCategories) {
+		this.encheresDaoArticlesVendus = encheresDaoArticlesVendus;
+		this.encheresDaoCategories = encheresDaoCategories;
+	}
+
+
+
+	@Override
+	public ArticleVendu mapRow(ResultSet rs, int rowNum) throws SQLException {
+		ArticleVendu article =new ArticleVendu();
+		article.setNo_article(rs.getInt(1));
+		article.setNom_article(rs.getString(2));
+		article.setDescription(rs.getString(3));
+		article.setDate_debut_encheres(rs.getObject(4, LocalDate.class));
+		article.setDate_fin_encheres(rs.getObject(5, LocalDate.class));
+		article.setPrix_initial(rs.getInt(6));
+		article.setPrix_vente(rs.getInt(7));
+		System.out.println(rs.getInt(8));
+		article.setCategorie(encheresDaoCategories.getCategorieById(rs.getInt(8)));
+		
+		return article;
+	}
+	
+	
+
+}
