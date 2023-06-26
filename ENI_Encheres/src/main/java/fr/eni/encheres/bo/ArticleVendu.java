7069e4f8ed23937ddbbaa43c265a014276f09983
tahina98 <tahina.foucher2023@campus-eni.fr>
2023-06-26 12:19:17 +0200

    bo articleVendu

--- ENI_Encheres/src/main/java/fr/eni/encheres/bo/ArticleVendu.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/bo/ArticleVendu.java
@@ -1,5 +1,101 @@
 package fr.eni.encheres.bo;
 
+import java.time.LocalDate;
+
 public class ArticleVendu {
 
+
+	//constantes etatVente
+	private final static int CREEE = 0;
+	private final static int EN_COURS = 1;
+	private final static int ENCHERES_TERMINEES = 2;
+	private final static int RETRAIT_EFFECTUE = 3;
+	
+	
+	
+	
+	private Integer no_article;
+	private String nom_article;
+	private String description;
+	private LocalDate date_debut_encheres;
+	private LocalDate date_fin_encheres;
+	private Integer prix_initial;
+	private Integer prix_vente;
+	private String etatVente;
+	
+	
+	//Constructeur vide
+	public ArticleVendu() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	//Getter Setter
+	public Integer getNo_article() {
+		return no_article;
+	}
+
+	public void setNo_article(Integer no_article) {
+		this.no_article = no_article;
+	}
+
+	public String getNom_article() {
+		return nom_article;
+	}
+
+	public void setNom_article(String nom_article) {
+		this.nom_article = nom_article;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDate_debut_encheres() {
+		return date_debut_encheres;
+	}
+
+	public void setDate_debut_encheres(LocalDate date_debut_encheres) {
+		this.date_debut_encheres = date_debut_encheres;
+	}
+
+	public LocalDate getDate_fin_encheres() {
+		return date_fin_encheres;
+	}
+
+	public void setDate_fin_encheres(LocalDate date_fin_encheres) {
+		this.date_fin_encheres = date_fin_encheres;
+	}
+
+	public Integer getPrix_initial() {
+		return prix_initial;
+	}
+
+	public void setPrix_initial(Integer prix_initial) {
+		this.prix_initial = prix_initial;
+	}
+
+	public Integer getPrix_vente() {
+		return prix_vente;
+	}
+
+	public void setPrix_vente(Integer prix_vente) {
+		this.prix_vente = prix_vente;
+	}
+
+	public String getEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+//Fin getter setter
+
+	
+	
 }
