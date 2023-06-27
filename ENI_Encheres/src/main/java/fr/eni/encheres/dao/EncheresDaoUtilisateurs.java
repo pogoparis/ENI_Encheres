f87b4710aa112d619faa59b219d364c91feba5e7
JuOkami <justin.candela@gmail.com>
2023-06-27 10:04:48 +0200

    nettoyage

--- ENI_Encheres/src/main/java/fr/eni/encheres/dao/EncheresDaoUtilisateurs.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/dao/EncheresDaoUtilisateurs.java
@@ -8,6 +8,7 @@ import fr.eni.encheres.bo.Utilisateur;
 public interface EncheresDaoUtilisateurs {
 	
 	List<Utilisateur> getAllUtilisateurs();
+	
 	Utilisateur getUtilisateurById(Integer idUtilisateur);
 
 }
