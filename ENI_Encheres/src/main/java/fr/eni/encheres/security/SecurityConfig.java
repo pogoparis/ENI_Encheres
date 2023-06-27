69c82da71c54644803c62daeeacdf68eefb34f46
tahina98 <tahina.foucher2023@campus-eni.fr>
2023-06-27 11:22:34 +0200

    Security envoie reussi

---
+++ ENI_Encheres/src/main/java/fr/eni/encheres/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package fr.eni.encheres.security;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain; 
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	protected final Log logger = LogFactory.getLog(getClass()); 
+	
+	@Bean
+	SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> {auth
+			.requestMatchers(HttpMethod.GET, "/encheres").permitAll()
+			.requestMatchers(HttpMethod.GET, "/").permitAll()
+			.requestMatchers(HttpMethod.GET, "/login").permitAll()
+			.requestMatchers(HttpMethod.GET, "/profil").hasRole("ADMIN");
+		});
+		
+		
+//		
+//		http.formLogin(form -> { 
+//
+//			form.loginPage("/login").permitAll(); 
+//
+//			form.defaultSuccessUrl("/"); 
+//
+//			}); 
+
+			 
+
+			return http.build(); 
+
+			} 
+		
+	}
+	
+	
+
