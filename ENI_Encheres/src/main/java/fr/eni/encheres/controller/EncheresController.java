58f28efa2752d0d1d3936ee23923aa4adcb701c1
tahina98 <tahina.foucher2023@campus-eni.fr>
2023-06-27 10:34:04 +0200

    Security à moitié

--- ENI_Encheres/src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -3,6 +3,7 @@ package fr.eni.encheres.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.eni.encheres.service.EncheresServiceArticlesVendus;
 
@@ -44,6 +45,18 @@ public class EncheresController {
 	public String afficherLogin() {
 		
 		return "login";
+	}
+	
+	@GetMapping("/profil")
+	public String afficherProfil() {
+		
+		return "profil";
+	}
+	
+	@PostMapping("/login")
+	public String login() {
+		
+		return "redirect:/encheres";
 	}
 	
 }
