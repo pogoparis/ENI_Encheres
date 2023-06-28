6fcb94ff8e0412bd04329f84e4e65e1379e95e29
JuOkami <justin.candela@gmail.com>
2023-06-28 10:24:00 +0200

    Page profil qui ne fonctionne pas (je push de la merde)

--- ENI_Encheres/src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -1,10 +1,13 @@
 package fr.eni.encheres.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Retrait;
@@ -13,20 +16,14 @@ import fr.eni.encheres.service.EncheresServiceArticlesVendus;
 import fr.eni.encheres.service.EncheresServiceCategorie;
 import fr.eni.encheres.service.EncheresServiceUtilisateur;
 
-
-
 @Controller
+@SessionAttributes ({"utilisateurConnecte"})
 public class EncheresController {
 	
 	EncheresServiceArticlesVendus encheresServiceArticlesVendus;
 	EncheresServiceCategorie encheresServiceCategorie;
 	EncheresServiceUtilisateur encheresServiceUtilisateur;
 
-	
-	
-
-	
-	
 	public EncheresController(EncheresServiceArticlesVendus encheresServiceArticlesVendus, EncheresServiceCategorie encheresServiceCategorie,EncheresServiceUtilisateur encheresServiceUtilisateur) {
 		this.encheresServiceArticlesVendus = encheresServiceArticlesVendus;
 		this.encheresServiceCategorie = encheresServiceCategorie;
@@ -36,31 +33,28 @@ public class EncheresController {
 	@GetMapping("/encheres")
 	public String afficherAccueil(Model model) {
 		model.addAttribute("listeArticles", encheresServiceArticlesVendus.findAllArticleVendu());
-		
-		
-		
 		return "index";
-		
 	}
 	
 	@GetMapping("/")
 	public String afficherRien() {
-		
-		
-		
 		return "redirect:/encheres";
-	
-}
+	}
 	
 	@GetMapping("/login")
 	public String afficherLogin() {
-		
 		return "login";
 	}
 	
-	@GetMapping("/profil")
+	@GetMapping("/inscription")
 	public String afficherProfil(@ModelAttribute Utilisateur utilisateur) {
-		
+		return "profil";
+	}
+	
+	@GetMapping("/profil")
+	public String afficherProfil(@ModelAttribute("utilisateurConnecte") Utilisateur utilisateurConnecte, Principal principal , Model model) {
+		System.out.println("PRINCIPAL"+principal.getName());
+		model.addAttribute("utilisateur", utilisateurConnecte);
 		return "profil";
 	}
 	
@@ -74,7 +68,6 @@ public class EncheresController {
 	@GetMapping("/creerarticle")
 	public String creerArticle(@ModelAttribute ArticleVendu articleVendu,@ModelAttribute Retrait retrait ,Model model) {
 		model.addAttribute("categories", encheresServiceCategorie.findAllCategorie());
-		
 		return "creerarticle";
 	}
 	
