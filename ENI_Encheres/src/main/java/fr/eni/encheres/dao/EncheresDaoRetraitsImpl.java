1da879a7bc18a56ed1488630259c3b8953784206
pogoparis <pogoparis@gmail.com>
2023-06-28 16:37:53 +0200

    création de la méthode create Retrait

--- ENI_Encheres/src/main/java/fr/eni/encheres/dao/EncheresDaoRetraitsImpl.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/dao/EncheresDaoRetraitsImpl.java
@@ -1,5 +1,8 @@
 package fr.eni.encheres.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,13 +13,23 @@ import fr.eni.encheres.bo.Retrait;
 public class EncheresDaoRetraitsImpl implements EncheresDaoRetraits {
 
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	final static String INSERT_RETRAIT = "INSERT INTO RETRAITS (rue, code_postal, ville, no_article VALUES (:rue, :code_postal, :ville, :no_article) ";
 	
 	
-	
+	public EncheresDaoRetraitsImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+	}
+
+
+
 	@Override
 	public void createRetrait(Retrait retrait, ArticleVendu article) {
-		
-		
+		Map<String, Object> map = new HashMap<>();		
+		map.put("rue", retrait.getRue());
+		map.put("code_postal", retrait.getCode_postal());
+		map.put("ville", retrait.getVille());
+		map.put("no_article", retrait.getArticle().getNo_article());
+		namedParameterJdbcTemplate.update(INSERT_RETRAIT, map);
 	}
 
 }
