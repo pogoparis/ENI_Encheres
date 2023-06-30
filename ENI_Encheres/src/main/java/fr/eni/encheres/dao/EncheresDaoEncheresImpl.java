2b257f8feed2adaac51a95c2ee281f02c0481ba0
JuOkami <justin.candela@gmail.com>
2023-06-30 17:26:30 +0200

    Mise a jour de la methode save encheres

--- ENI_Encheres/src/main/java/fr/eni/encheres/dao/EncheresDaoEncheresImpl.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/dao/EncheresDaoEncheresImpl.java
@@ -16,6 +16,7 @@ public class EncheresDaoEncheresImpl implements EncheresDaoEncheres {
 	
 	public final static String INSERT_ENCHERES ="INSERT INTO ENCHERES (date_enchere, montant_enchere, no_article, no_utilisateur) VALUES (:date_enchere, :montant_enchere, :no_article, :no_utilisateur)";
 	public final static String SELECT_ENCHERES_BY_ARTICLE = "select encheres.no_utilisateur, encheres.no_article, date_enchere, montant_enchere from ENCHERES inner join ARTICLES_VENDUS on ENCHERES.no_article = ARTICLES_VENDUS.no_article where ARTICLES_VENDUS.no_article =:no_article";
+	public final static String UPDATE_ENCHERE = "update ENCHERES set date_enchere=:date_enchere, montant_enchere=:montant_enchere where no_utilisateur=:no_utilisateur";
 	
 	private EncheresDaoUtilisateurs encheresDaoUtilisateurs;
 	private EncheresDaoArticlesVendus encheresDaoArticlesVendus;
@@ -39,7 +40,20 @@ public class EncheresDaoEncheresImpl implements EncheresDaoEncheres {
 		enchereMap.addValue("montant_enchere", enchere.getMontant_enchere());
 		enchereMap.addValue("no_article", enchere.getArticle().getNo_article());
 		enchereMap.addValue("no_utilisateur", enchere.getUtilisateur().getNo_utilisateur());	
-		namedParameterJdbcTemplate.update(INSERT_ENCHERES, enchereMap);	
+		
+		Boolean dejaEncherit = false;
+		for (Enchere enchereDejaExistante : findEncheresByArticle(enchere.getArticle())) {
+			if (enchere.getUtilisateur().getNo_utilisateur() == enchereDejaExistante.getUtilisateur().getNo_utilisateur()) {
+				dejaEncherit = true;
+				break;
+			} 
+			dejaEncherit = false;
+			}
+		if (dejaEncherit) {
+		namedParameterJdbcTemplate.update(UPDATE_ENCHERE, enchereMap);
+		} else {
+		namedParameterJdbcTemplate.update(INSERT_ENCHERES, enchereMap);
+		}
 	}
 	
 	public List<Enchere> findEncheresByArticle(ArticleVendu article){
