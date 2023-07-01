0dff6d7ab97423707f69192f1fd608b01396b384
pogoparis <pogoparis@gmail.com>
2023-07-01 15:27:00 +0200

    Ajout d'un controller Connection (login, register et profil)

---
+++ ENI_Encheres/src/main/java/fr/eni/encheres/controller/ConnectController.java
@@ -0,0 +1,64 @@
+package fr.eni.encheres.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.service.EncheresServiceUtilisateur;
+
+@Controller
+public class ConnectController {
+
+	EncheresServiceUtilisateur encheresServiceUtilisateur;
+	
+	// CONSTRUCTEUR
+	public ConnectController(EncheresServiceUtilisateur encheresServiceUtilisateur) {
+		super();
+		this.encheresServiceUtilisateur = encheresServiceUtilisateur;
+	}
+
+	/*********** AFFICHAGE PAGE LOGIN ****************/
+	@GetMapping("/login")
+	public String afficherLogin(Principal principal) {
+		if (principal != null) {
+			return "redirect:/encheres";
+		}
+		return "login";
+	}
+	
+	/************** AFFICHAGE PAGE INSCRIPTION **************/
+	@GetMapping("/inscription")
+	public String afficherProfil(@ModelAttribute Utilisateur utilisateur) {
+		return "profil";
+	}
+	
+	/************* BOUTON VALIDATION REGISTER *****************/
+	@PostMapping("/register")
+	public String register(@ModelAttribute Utilisateur utilisateur) {
+		encheresServiceUtilisateur.createUtilisateur(utilisateur);
+		return "redirect:/login";
+	}
+
+	/****************** AFFICHAGE DETAIL UTILISATEUR (DETAIL)*******************/
+	@GetMapping("/details")
+	public String afficherDetails(String pseudo, Model model, Principal principal) {
+		Utilisateur utilisateur = encheresServiceUtilisateur.findUserByPseudo(pseudo);
+		model.addAttribute("utilisateur", utilisateur);
+		model.addAttribute("pseudoConnecte", principal.getName());
+		return "details";
+	}
+	
+
+	/*********** AFFFICHAGE PAGE PROFIL (MODIF/CREATION) *****************/
+	@GetMapping("/profil")
+	public String afficherProfil(Principal principal, Model model) {
+		Utilisateur utilisateurconnecte = encheresServiceUtilisateur.findUserByPseudo(principal.getName());
+		model.addAttribute("utilisateur", utilisateurconnecte);
+		return "profil";
+	}
+}
--- ENI_Encheres/src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ ENI_Encheres/src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -6,7 +6,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.Principal;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import fr.eni.encheres.bo.ArticleVendu;
-import fr.eni.encheres.bo.Categorie;
 import fr.eni.encheres.bo.Enchere;
 import fr.eni.encheres.bo.Retrait;
 import fr.eni.encheres.bo.Utilisateur;
@@ -27,18 +25,15 @@ import fr.eni.encheres.service.EncheresServiceUtilisateur;
 
 @Controller
 public class EncheresController {
-	
+
 	EncheresServiceArticlesVendus encheresServiceArticlesVendus;
 	EncheresServiceCategorie encheresServiceCategorie;
 	EncheresServiceUtilisateur encheresServiceUtilisateur;
 	EncheresServiceRetrait encheresServiceRetrait;
 	EncheresServiceEncheres encheresServiceEncheres;
-	
-	public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") +  "/src/main/resources/static/Images";
-	//private static final String UPLOAD_DIR ="/Images";
-	
-	
+	public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static/Images";
 
+	// CONSTRUCTEUR
 	public EncheresController(EncheresServiceArticlesVendus encheresServiceArticlesVendus,
 			EncheresServiceCategorie encheresServiceCategorie, EncheresServiceUtilisateur encheresServiceUtilisateur,
 			EncheresServiceRetrait encheresServiceRetrait, EncheresServiceEncheres encheresServiceEncheres) {
@@ -49,93 +44,64 @@ public class EncheresController {
 		this.encheresServiceEncheres = encheresServiceEncheres;
 	}
 
-	@PostMapping("/encherir")
-	public String encherir(@ModelAttribute Enchere enchere,  Model model ) {
-		System.out.println("ENCHERES "+enchere);
-		encheresServiceEncheres.creationEncheres(enchere);
-		encheresServiceArticlesVendus.majPrixArticle(enchere);
-		encheresServiceUtilisateur.majCreditUtilisateur(enchere);
-		return "redirect:/article?id="+enchere.getArticle().getNo_article();
-	}
-	
-	
+	/************ AFFICHAGE ACCUEIL ********************/
 	@GetMapping("/encheres")
 	public String afficherAccueil(Model model) {
 		model.addAttribute("listeCategorie", encheresServiceCategorie.findAllCategorie());
 		model.addAttribute("listeArticles", encheresServiceArticlesVendus.findAllArticleVendu());
 		return "index";
 	}
-	
+
 	@GetMapping("/")
 	public String afficherRien() {
 		return "redirect:/encheres";
 	}
-	
-	@GetMapping("/login")
-	public String afficherLogin(Principal principal) {
-		if (principal != null ) {
-			return "redirect:/encheres";
-		} 	
-			return "login";	
-	}
-	
-	@GetMapping("/inscription")
-	public String afficherProfil(@ModelAttribute Utilisateur utilisateur) {
-		return "profil";
-	}
-	
-	@GetMapping("/profil")
-	public String afficherProfil(Principal principal , Model model) {
-		Utilisateur utilisateurconnecte = encheresServiceUtilisateur.findUserByPseudo(principal.getName());
-		model.addAttribute("utilisateur", utilisateurconnecte);
-		return "profil";
-	}
-	
-	@PostMapping("/register")
-	 public String register( @ModelAttribute Utilisateur utilisateur ) {
-		encheresServiceUtilisateur.createUtilisateur(utilisateur);
-        return "redirect:/login";
-    }
-	
+
+	/************* AFFICAHGE CREATION ARTICLE ******************/
 	@GetMapping("/creerarticle")
-	public String creerArticle(@ModelAttribute ArticleVendu articleVendu,@ModelAttribute Retrait retrait ,Model model) {
+	public String creerArticle(@ModelAttribute ArticleVendu articleVendu, @ModelAttribute Retrait retrait,
+			Model model) {
 		model.addAttribute("categories", encheresServiceCategorie.findAllCategorie());
 		return "creerarticle";
 	}
-	
+
+	/********** BOUTON VALIDATION CREATION ENCHERE ***********/
+	@PostMapping("/encherir")
+	public String encherir(@ModelAttribute Enchere enchere, Model model) {
+		encheresServiceEncheres.creationEncheres(enchere);
+		encheresServiceArticlesVendus.majPrixArticle(enchere);
+		encheresServiceUtilisateur.majCreditUtilisateur(enchere);
+		return "redirect:/article?id=" + enchere.getArticle().getNo_article();
+	}
+
+	/*********** BOUTON VALIDATION CREATION ARTICLE ***********/
 	@PostMapping("/creationarticle")
-	public String creationArticle(@RequestParam("image") MultipartFile file, @ModelAttribute ArticleVendu articleVendu,@ModelAttribute Retrait retrait, Principal principal, Model model) throws IOException {
+	public String creationArticle(@RequestParam("image") MultipartFile file, @ModelAttribute ArticleVendu articleVendu,
+			@ModelAttribute Retrait retrait, Principal principal, Model model) throws IOException {
 		articleVendu.setUtilisateur(encheresServiceUtilisateur.findUserByPseudo(principal.getName()));
 		encheresServiceArticlesVendus.createArticle(articleVendu);
 		encheresServiceRetrait.createRetrait(retrait, articleVendu);
-		//images
+		// images
 		StringBuilder fileNames = new StringBuilder();
-        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, (articleVendu.getNo_article().toString()+".jpg") );
-        fileNames.append(articleVendu.getNo_article()+".jpg");
-        Files.write(fileNameAndPath, file.getBytes());
-        model.addAttribute("msg", fileNames.toString());
+		Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, (articleVendu.getNo_article().toString() + ".jpg"));
+		fileNames.append(articleVendu.getNo_article() + ".jpg");
+		Files.write(fileNameAndPath, file.getBytes());
+		model.addAttribute("msg", fileNames.toString());
 		return "redirect:/encheres";
 	}
-	
-	@GetMapping("/details")
-	public String afficherDetails(String pseudo,Model model) {
-		
-		Utilisateur utilisateur = encheresServiceUtilisateur.findUserByPseudo(pseudo);
-		model.addAttribute("utilisateur", utilisateur);
-		return "details";
-	}
-	
+
+	/****************** AFFICHAGE PAGE DETAIL ARTICLE ***************/
 	@GetMapping("/article")
-	public String afficherDetailsArticles(Integer id, Model model, Principal principal, @ModelAttribute Enchere enchere) {
+	public String afficherDetailsArticles(Integer id, Model model, Principal principal,
+			@ModelAttribute Enchere enchere) {
 		ArticleVendu article = encheresServiceArticlesVendus.findArticleById(id);
 		Retrait retrait = encheresServiceRetrait.findRetraitByArticle(article);
 		Enchere meilleureEnchere = encheresServiceEncheres.getMeilleureEnchereByArticle(article);
 		
-		if(principal!=null) {
+		if (principal != null) {
 			model.addAttribute("user", encheresServiceUtilisateur.findUserByPseudo(principal.getName()));
-			
+
 		}
-		System.out.println("ENCHERE PRE VALIDATION"+enchere);
 		model.addAttribute("article", article);
 		model.addAttribute("retrait", retrait);
 		model.addAttribute("meilleureEnchere", meilleureEnchere);
@@ -143,27 +109,15 @@ public class EncheresController {
 		model.addAttribute("tokenAffichage", tokenAffichage);
 		return "article";
 	}
-	
-	// Methode En GettMaping Recherche par nom
+
+	/************** BOUTON RECHERCHE GET *****************/
 	@GetMapping("/recherche")
 	public String recherche(String rechercheNom, Model model) {
 		model.addAttribute("listeCategorie", encheresServiceCategorie.findAllCategorie());
 		List<ArticleVendu> listeArticles = encheresServiceArticlesVendus.findArticleContainNom(rechercheNom);
 		model.addAttribute("listeArticles", listeArticles);
-		
 		return "index";
 	}
-	
-	
-	
-//	@PostMapping("/upload") 
-//    public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
-//        StringBuilder fileNames = new StringBuilder();
-//        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
-//        fileNames.append(file.getOriginalFilename());
-//        Files.write(fileNameAndPath, file.getBytes());
-//        model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
-//        return "redirect:/creerarticle";
-//    }
-	
+
+
 }
