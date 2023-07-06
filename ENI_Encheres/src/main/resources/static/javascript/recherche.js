f5075096dc9c70dc368943e6f579f45f5b90e59b
pogoparis <pogoparis@gmail.com>
2023-07-06 19:58:35 +0200

    javascript fadout message connecte/deco

--- ENI_Encheres/src/main/resources/static/javascript/recherche.js
+++ ENI_Encheres/src/main/resources/static/javascript/recherche.js
@@ -1,55 +1,54 @@
 window.onload = init;
 
 
+$(document).ready(function() {
+	var message = $(".alert");
 
-function init(){
+	function masquerDiv() {
+		message.fadeOut(3000, function() {
+			// message.remove();
+		});
+	}
+
+	setTimeout(masquerDiv, 4000);
+});
 
 
-//document.addEventListener('DOMContentLoaded', function() {
-//		  const isFirstVisit = !document.cookie.includes('visited=true');
-//		  const targetElement = document.querySelector('#page2');
-//		  const currentURL = window.location.href;
-//
-//		  // Vérifier si l'URL contient des segments supplémentaires après l'URL de base
-//		  const hasAdditionalSegments = currentURL !== window.location.origin + window.location.pathname;
-//
-//		  if (!isFirstVisit && targetElement && hasAdditionalSegments) {
-//		    targetElement.scrollIntoView();
-//		  }
-//
-//		  // Définir le cookie pour indiquer que l'utilisateur a visité le site
-//		  if (isFirstVisit) {
-//		    document.cookie = 'visited=true; max-age=3100'; 
-//		  }
-//		});
 
-	
-		var optionVentes = document.querySelector('input[value="ventes"]');
-		var optionAchats = document.querySelector('input[value="achats"]');
-		var checkboxes = document.querySelectorAll('.venteCheck');
-		var checkboxes2 = document.querySelectorAll('.disable2');
+function init() {
 
-		optionVentes.addEventListener('change', function () {
-			if (optionVentes.checked) {	
-				checkboxes.forEach(function (checkbox) {
+
+	var optionVentes = document.querySelector('input[value="ventes"]');
+	var optionAchats = document.querySelector('input[value="achats"]');
+	var checkboxes = document.querySelectorAll('.venteCheck');
+	var checkboxes2 = document.querySelectorAll('.disable2');
+
+	if (optionVentes) {
+		optionVentes.addEventListener('change', function() {
+			if (optionVentes.checked) {
+				checkboxes.forEach(function(checkbox) {
 					checkbox.disabled = false;
 				});
-				checkboxes2.forEach(function (checkbox) {
+				checkboxes2.forEach(function(checkbox) {
 					checkbox.disabled = true;
 				});
 			}
 		});
+	}
 
-		optionAchats.addEventListener('change', function () {
+	if (optionAchats) {
+		optionAchats.addEventListener('change', function() {
 			if (optionAchats.checked) {
-				checkboxes.forEach(function (checkbox) {
+				checkboxes.forEach(function(checkbox) {
 					checkbox.disabled = true;
 				});
-				checkboxes2.forEach(function (checkbox) {
+				checkboxes2.forEach(function(checkbox) {
 					checkbox.disabled = false;
 				});
 			}
 		});
-		
-		
-		}
+
+	}
+
+
+}
